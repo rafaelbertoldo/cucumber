4437fde9508376b4fb4dbfa35050263e3480edf0
Ricardo Fahham <rfahham@hotmail.com>
2025-02-14 19:39:53 -0300

    Adicionando mais um cenário: dados são obrigatórios

--- features/login.feature
+++ features/login.feature
@@ -9,3 +9,8 @@ Feature: Teste de Login
     Given que o usuário está na página de login
     When o usuário insere o nome de usuário "usuario@example.com" e a senha "senha_errada"
     Then o sistema deve exibir uma mensagem de erro de login
+  
+  Scenario: Tentar acessar a página sem as credenciais
+    Given que o usuário está na página de login
+    When o usuário não insere o nome de usuário e senha
+    Then o sistema deve exibir uma mensagem de erro informando que os dados são obrigatórios
--- features/steps/loginSteps.js
+++ features/steps/loginSteps.js
@@ -14,30 +14,58 @@ Before(async function () {
 
 Given('que o usuário está na página de login', async function () {
   await driver.get('https://horadoqa.github.io/login/');
+  await driver.sleep(2000);
 });
+
 
 When('o usuário insere o nome de usuário {string} e a senha {string}', async function (username, password) {
   const usernameField = await driver.findElement(By.id('username'));
   const passwordField = await driver.findElement(By.id('password'));
   const loginButton = await driver.findElement(By.id('button'));
-
+  
   await usernameField.sendKeys(username);
   await passwordField.sendKeys(password);
+  await driver.sleep(2000);
   await loginButton.click();
 });
+
 
 Then('o usuário deve ser redirecionado para a página Bem-Vindo', async function () {
   const welcomeMessage = await driver.wait(until.elementIsVisible(driver.findElement(By.id('inicio'))), 5000);
   const messageText = await welcomeMessage.getText();
-  console.log("Mensagem de boas-vindas:", messageText);
+  // console.log("Mensagem de boas-vindas:", messageText);
   expect(messageText).to.include('Bem-vindo');
+  await driver.sleep(2000);
 });
-
 
 Then('o sistema deve exibir uma mensagem de erro de login', async function () {
   const errorMessage = await driver.findElement(By.id('error-message'));
   const errorText = await errorMessage.getText();
+  // console.log("Mensagem informando o erro:", errorText);
   expect(errorText).to.include('E-mail ou senha inválidos!');
+  await driver.sleep(2000);
+});
+
+
+When('o usuário não insere o nome de usuário e senha', async function () {
+  const usernameField = await driver.findElement(By.id('username'));
+  const passwordField = await driver.findElement(By.id('password'));
+  const loginButton = await driver.findElement(By.id('button'));
+  
+  // Deixar os campos vazios, sem preencher
+  await usernameField.clear();
+  await passwordField.clear();
+  
+  // Não clicar no botão de login (assumindo que ele só será clicado se os campos estiverem preenchidos)
+  await loginButton.click();
+  await driver.sleep(2000);
+});
+
+Then('o sistema deve exibir uma mensagem de erro informando que os dados são obrigatórios', async function () {
+  const errorMessage = await driver.wait(until.elementLocated(By.id('error-message')), 5000);
+  const errorText = await errorMessage.getText();
+  // console.log("Mensagem informando o erro:", errorText);
+  expect(errorText).to.include('E-mail e senha são obrigatórios!');
 });
 
 // Fechar o navegador após cada cenário
